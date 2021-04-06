9eaa59a99b3eb9dc58aa0764d17a2e8f3ebb558b
Bartłomiej Klocek <barthap10@gmail.com>
2021-04-06 09:25:35 +0200

    Update DB base schema

--- db/data.sql
+++ db/data.sql
@@ -5,26 +5,26 @@ values ('S', 'Story'),
        ('P', 'Person'),
        ('I', 'Photo/Video');
 
-insert into Items (ItemId, TypeCode, CreatedDateTime, UpdatedDateTime)
-values ('b81763dc-4b7e-4af7-ad64-2af50cb74666', 'S', now(), now()), -- 1
-       ('9477d81a-f324-4835-bea0-7b256ae37069', 'F', now(), now()), -- 2
-       ('22526b6c-0667-4c08-b58c-823b098a94a9', 'F', now(), now()), -- 3
-       ('e20e5eed-dc07-463f-b763-3e508d994353', 'I', now(), now()), -- 4
-       ('d97be831-2e4d-4e85-9b48-f0aa00705e07', 'P', now(), now()), -- 5
-       ('a58e9971-365e-4ba0-94f5-a5ac01a43790', 'D', now(), now()), -- 6
-       ('86048604-74cd-492e-b93e-5f347cbead4c', 'S', now(), now()), -- 7
-       ('87c8aeda-71aa-46ea-9b94-ea759bebf8ae', 'P', now(), now()), -- 8
-       ('e93ae5e0-8e5d-4329-b60a-fcb7f4aab109', 'P', now(), now()), -- 9
-       ('f8199f27-303f-47e1-904c-abcd117b321a', 'D', now(), now()), -- 10
-       ('5503ff00-21e8-47c4-87d6-803e50d50d3f', 'F', now(), now()), -- 11
-       ('31b9dc34-982e-432d-acef-5d526fe25da5', 'P', now(), now()), -- 12
-       ('f02b2d8e-cb30-4a1f-b33c-395736cb1c93', 'I', now(), now()), -- 13
-       ('d0a74e3d-707c-4447-8d8e-d42f27da207b', 'I', now(), now()), -- 14
-       ('08beec18-4593-4a43-b404-739a09fa6633', 'I', now(), now()), -- 15
-       ('b9773f6e-12ea-4c9d-8393-06c0a5b8a1c2', 'I', now(), now()), -- 16
-       ('bf604923-ea85-41d8-93c6-a37bc0485649', 'I', now(), now()); -- 17
+insert into Items (item_id, type_code)
+values ('b81763dc-4b7e-4af7-ad64-2af50cb74666', 'S'), -- 1
+       ('9477d81a-f324-4835-bea0-7b256ae37069', 'F'), -- 2
+       ('22526b6c-0667-4c08-b58c-823b098a94a9', 'F'), -- 3
+       ('e20e5eed-dc07-463f-b763-3e508d994353', 'I'), -- 4
+       ('d97be831-2e4d-4e85-9b48-f0aa00705e07', 'P'), -- 5
+       ('a58e9971-365e-4ba0-94f5-a5ac01a43790', 'D'), -- 6
+       ('86048604-74cd-492e-b93e-5f347cbead4c', 'S'), -- 7
+       ('87c8aeda-71aa-46ea-9b94-ea759bebf8ae', 'P'), -- 8
+       ('e93ae5e0-8e5d-4329-b60a-fcb7f4aab109', 'P'), -- 9
+       ('f8199f27-303f-47e1-904c-abcd117b321a', 'D'), -- 10
+       ('5503ff00-21e8-47c4-87d6-803e50d50d3f', 'F'), -- 11
+       ('31b9dc34-982e-432d-acef-5d526fe25da5', 'P'), -- 12
+       ('f02b2d8e-cb30-4a1f-b33c-395736cb1c93', 'I'), -- 13
+       ('d0a74e3d-707c-4447-8d8e-d42f27da207b', 'I'), -- 14
+       ('08beec18-4593-4a43-b404-739a09fa6633', 'I'), -- 15
+       ('b9773f6e-12ea-4c9d-8393-06c0a5b8a1c2', 'I'), -- 16
+       ('bf604923-ea85-41d8-93c6-a37bc0485649', 'I'); -- 17
 
-insert into Story (StoryId, StoryDateTime, Header, Content)
+insert into Story (story_id, StoryDateTime, Header, Content)
 values ('b81763dc-4b7e-4af7-ad64-2af50cb74666', DATE('2015-07-12'), 'Sample story', '_**Hello, this is an example story that never happened!**_
 ​
 Ut non ex eu velit lobortis cursus. Nulla sodales justo id nisl dapibus fringilla. Curabitur sit amet nisl eget sem porta sagittis. Ut iaculis imperdiet posuere. Praesent nulla mi, varius quis vulputate nec, dictum eget felis. Praesent non felis ut massa sodales lacinia. Pellentesque nec nulla turpis. Donec scelerisque varius ipsum quis tincidunt. Donec lacinia, magna lobortis euismod luctus, augue diam maximus arcu, id finibus lacus ante eget elit. Duis aliquet felis sit amet velit porta facilisis. Pellentesque eget gravida orci. Etiam tempor nibh sed est sodales, a scelerisque erat auctor. Mauris feugiat elit congue, accumsan odio ac, varius ipsum. Fusce pellentesque ligula a nisi congue sodales. Phasellus porta dui in lectus dictum, tincidunt vehicula nisl consectetur. Duis ultricies sodales tempus.
@@ -36,12 +36,12 @@ Some _example story content_
  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pellentesque leo arcu, et iaculis dui aliquet eu. In et magna eget elit placerat posuere. Mauris sodales, ante vitae cursus bibendum, mi dui venenatis tortor, a porta urna lectus at ante. Etiam porta, quam vitae dignissim rhoncus, augue est bibendum quam, ac varius nisl lacus at velit. Sed vestibulum libero ac sagittis venenatis. Integer non nibh eros. Donec laoreet pulvinar diam, in maximus libero vestibulum eget. Quisque a ultricies neque. Vestibulum consequat orci et ultricies accumsan. Donec volutpat orci neque, gravida scelerisque urna pretium vitae. Nulla volutpat, massa nec consectetur tristique, eros arcu interdum sapien, sodales vulputate erat sem vel risus. Praesent dolor diam, tempus sed arcu eget, maximus vestibulum justo. Aliquam ornare sollicitudin fringilla. Vivamus semper, purus non vestibulum varius, mi felis egestas sem, non sodales magna turpis non lacus. Cras arcu dui, iaculis a aliquam et, cursus a ipsum. In rhoncus volutpat iaculis.
 ');
 
-insert into Files (FileId, Filename, Title, Description)
+insert into Files (file_id, Filename, Title, Description)
 values ('9477d81a-f324-4835-bea0-7b256ae37069', 'file.tmp', 'File 1', 'This file actually **does not** exist'),
        ('22526b6c-0667-4c08-b58c-823b098a94a9', 'file2.tmp', 'File 2', 'Other temp _non-existing file'),
        ('5503ff00-21e8-47c4-87d6-803e50d50d3f', 'archive.zip', 'A Zip archive', 'Archive description');
 
-insert into Photos (PhotoId, Filename, Title, Description)
+insert into Photos (photo_id, Filename, Title, Description)
 values ('e20e5eed-dc07-463f-b763-3e508d994353', 'example-photo-1.jpg', 'Example photo', 'This is a photo description.
 The photo has been downloaded from [https://unsplash.com/search/photos/sample](https://unsplash.com/search/photos/sample)
 '),
@@ -64,7 +64,7 @@ The photo has been downloaded from [https://unsplash.com/search/photos/sample](h
 ');
 
 
-insert into People (PersonId, FullName, BirthDate, Description)
+insert into People (person_id, full_name, birth_date, Description)
 values ('d97be831-2e4d-4e85-9b48-f0aa00705e07', 'John Smith', '1979-10-29', 'Example _person_'),
        ('87c8aeda-71aa-46ea-9b94-ea759bebf8ae', 'Benjamin Henderson', '1981-05-14',
         'Person created using random name generator'),
@@ -72,7 +72,7 @@ values ('d97be831-2e4d-4e85-9b48-f0aa00705e07', 'John Smith', '1979-10-29', 'Exa
        ('31b9dc34-982e-432d-acef-5d526fe25da5', 'Samuel Austin', '1990-12-11', 'This name is also generated, [https://www.fantasynamegenerators.com/english_names.php](https://www.fantasynamegenerators.com/english_names.php)
 ');
 
-insert into Documents (DocumentId, Title, Type, Filename, Content)
+insert into Documents (document_id, Title, Type, Filename, Content)
 VALUES ('a58e9971-365e-4ba0-94f5-a5ac01a43790', 'Task list', 'EXTERNAL', 'tasks.docx', null),
        ('f8199f27-303f-47e1-904c-abcd117b321a', 'Embedded document', 'EMBEDDED', null, '## Document title
 Documents could also be **embedded** and use _markdown_ syntax!
@@ -81,7 +81,7 @@ Documents could also be **embedded** and use _markdown_ syntax!
  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pellentesque leo arcu, et iaculis dui aliquet eu. In et magna eget elit placerat posuere. Mauris sodales, ante vitae cursus bibendum, mi dui venenatis tortor, a porta urna lectus at ante. Etiam porta, quam vitae dignissim rhoncus, augue est bibendum quam, ac varius nisl lacus at velit. Sed vestibulum libero ac sagittis venenatis. Integer non nibh eros. Donec laoreet pulvinar diam, in maximus libero vestibulum eget. Quisque a ultricies neque. Vestibulum consequat orci et ultricies accumsan. Donec volutpat orci neque, gravida scelerisque urna pretium vitae. Nulla volutpat, massa nec consectetur tristique, eros arcu interdum sapien, sodales vulputate erat sem vel risus. Praesent dolor diam, tempus sed arcu eget, maximus vestibulum justo. Aliquam ornare sollicitudin fringilla. Vivamus semper, purus non vestibulum varius, mi felis egestas sem, non sodales magna turpis non lacus. Cras arcu dui, iaculis a aliquam et, cursus a ipsum. In rhoncus volutpat iaculis.
 ');
 
-insert into Reference (SourceId, TargetId)
+insert into Reference (source_id, target_id)
 values ('d97be831-2e4d-4e85-9b48-f0aa00705e07', 'f8199f27-303f-47e1-904c-abcd117b321a'),
        ('d0a74e3d-707c-4447-8d8e-d42f27da207b', 'b9773f6e-12ea-4c9d-8393-06c0a5b8a1c2'),
        ('5503ff00-21e8-47c4-87d6-803e50d50d3f', '87c8aeda-71aa-46ea-9b94-ea759bebf8ae'),
--- db/schema.sql
+++ db/schema.sql
@@ -4,215 +4,215 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 drop table if exists Type cascade;
 create table Type
 (
-    TypeCode CHAR(1)  NOT NULL,
-    Name     CHAR(30) NOT NULL,
-    CONSTRAINT Type_PK PRIMARY KEY (TypeCode),
+    type_code CHAR(1)  NOT NULL,
+    Name     varCHAR(30) NOT NULL,
+    CONSTRAINT Type_PK PRIMARY KEY (type_code),
     CONSTRAINT Type_AK UNIQUE (Name)
 );
 
 -- ENTITY BASE - ITEM
--- Only ItemID has auto-generated ID, all other entity types have same ID as Item
+-- Only item_id has auto-generated ID, all other entity types have same ID as Item
 drop table if exists Items cascade;
 create table Items
 (
-    ItemId          uuid DEFAULT uuid_generate_v4(),
-    TypeCode        CHAR(1)   not null,
-    CreatedDateTime timestamp not null,
-    UpdatedDateTime timestamp not null,
+    item_id          uuid DEFAULT uuid_generate_v4(),
+    type_code        CHAR(1)   not null,
+    created_at timestamp not null DEFAULT now(),
+    updated_at timestamp not null DEFAULT now(),
 
-    constraint Item_PK primary key (ItemId),
-    constraint Item_to_Type_FK foreign key (TypeCode) references Type (TypeCode)
+    constraint Item_PK primary key (item_id),
+    constraint Item_to_Type_FK foreign key (type_code) references Type (type_code)
 );
 
 -- ENTITY TYPES
 drop table if exists Story cascade;
 create table Story
 (
-    StoryId       uuid,
+    story_id       uuid,
     StoryDateTime timestamp not null,
     Header        varchar(255),
     Content       text      not null,
 
-    constraint Story_PK primary key (StoryId),
-    constraint Story_to_Item_FK foreign key (StoryId) references Items (ItemId) on delete cascade
+    constraint Story_PK primary key (story_id),
+    constraint Story_to_Item_FK foreign key (story_id) references Items (item_id) on delete cascade
 );
 
 drop table if exists Photos cascade;
 create table Photos
 (
-    PhotoId     uuid,
-    Filename    char(60) not null,
-    Title       char(60) not null,
+    photo_id     uuid,
+    Filename    varchar(60) not null,
+    Title       varchar(60) not null,
     Description text,
 
-    constraint Photo_PK primary key (PhotoId),
-    constraint Photo_to_Item_FK foreign key (PhotoId) references Items (ItemId) on delete cascade
+    constraint Photo_PK primary key (photo_id),
+    constraint Photo_to_Item_FK foreign key (photo_id) references Items (item_id) on delete cascade
 );
 
 drop table if exists People cascade;
 create table People
 (
-    PersonId    uuid,
-    FullName    char(50) not null,
-    BirthDate   date,
+    person_id    uuid,
+    full_name    varchar(50) not null,
+    birth_date   date,
     Description text,
 
-    constraint People_PK primary key (PersonId),
-    constraint People_to_Item_FK foreign key (PersonId) references Items (ItemId) on delete cascade
+    constraint People_PK primary key (person_id),
+    constraint People_to_Item_FK foreign key (person_id) references Items (item_id) on delete cascade
 );
 
 drop table if exists Documents;
 create table Documents
 (
-    DocumentId uuid,
+    document_id uuid,
     Title      varchar(50) not null,
     Type       varchar(20) not null, -- Type, raw Enum Value, can be embedded markdown or external word/pdf
     Filename   varchar(60),
     Content    text,
 
-    constraint Documents_PK primary key (DocumentId),
-    constraint Docs_to_Item_FK foreign key (DocumentId) references Items (ItemId) on delete cascade
+    constraint Documents_PK primary key (document_id),
+    constraint Docs_to_Item_FK foreign key (document_id) references Items (item_id) on delete cascade
 );
 
 drop table if exists Files cascade;
 create table Files
 (
-    FileId      uuid,
-    Filename    char(60) not null,
-    Title       char(60) not null,
+    file_id      uuid,
+    Filename    varchar(60) not null,
+    Title       varchar(60) not null,
     Description text,
 
-    constraint File_PK primary key (FileId),
-    constraint File_to_Item_FK foreign key (FileId) references Items (ItemId) on delete cascade
+    constraint File_PK primary key (file_id),
+    constraint File_to_Item_FK foreign key (file_id) references Items (item_id) on delete cascade
 );
 
 -- REFERENCE TABLE
 drop table if exists Reference cascade;
 create table Reference
 (
-    ReferenceId uuid DEFAULT uuid_generate_v4(),
-    SourceId    uuid not null,
-    TargetId    uuid not null,
+    reference_id uuid DEFAULT uuid_generate_v4(),
+    source_id    uuid not null,
+    target_id    uuid not null,
 
-    constraint Ref_PK primary key (ReferenceId),
-    constraint Ref_AK unique (SourceId, TargetId),
-    constraint Source_to_Item_FK foreign key (SourceId) references Items (ItemId) on delete cascade,
-    constraint Target_to_Item_FK foreign key (TargetId) references Items (ItemId) on delete cascade
+    constraint Ref_PK primary key (reference_id),
+    constraint Ref_AK unique (source_id, target_id),
+    constraint Source_to_Item_FK foreign key (source_id) references Items (item_id) on delete cascade,
+    constraint Target_to_Item_FK foreign key (target_id) references Items (item_id) on delete cascade
 );
 
 --ENTITY VIEWS - They are fetched by app
-drop view if exists StoryView;
-create view StoryView as
-select I.ItemId AS Id,
-       I.CreatedDateTime,
-       I.UpdatedDateTime,
+drop view if exists story_view;
+create view story_view as
+select I.item_id AS Id,
+       I.created_at,
+       I.updated_at,
        S.Header,
        S.StoryDateTime,
        S.Content
 from Items I
-         JOIN Story S on I.ItemId = S.StoryId;
+         JOIN Story S on I.item_id = S.story_id;
 
-drop view if exists FileView;
-create view FileView as
-select I.ItemId AS Id,
-       I.CreatedDateTime,
-       I.UpdatedDateTime,
+drop view if exists file_view;
+create view file_view as
+select I.item_id AS Id,
+       I.created_at,
+       I.updated_at,
        F.Title,
        F.Filename,
        F.Description
 from Items I
-         JOIN Files F on I.ItemId = F.FileId;
+         JOIN Files F on I.item_id = F.file_id;
 
 
-drop view if exists PhotoView;
-create view PhotoView as
-select I.ItemId AS Id,
-       I.CreatedDateTime,
-       I.UpdatedDateTime,
+drop view if exists photo_view;
+create view photo_view as
+select I.item_id AS Id,
+       I.created_at,
+       I.updated_at,
        P.Title,
        P.Filename,
        P.Description
 from Items I
-         JOIN Photos P on I.ItemId = P.PhotoId;
+         JOIN Photos P on I.item_id = P.photo_id;
 
 
-drop view if exists DocumentView;
-create view DocumentView as
-select I.ItemId as Id,
-       I.CreatedDateTime,
-       I.UpdatedDateTime,
+drop view if exists document_view;
+create view document_view as
+select I.item_id as Id,
+       I.created_at,
+       I.updated_at,
        D.Type,
        D.Title,
        D.Content,
        D.Filename
 from Items I
-         join Documents D on I.ItemId = D.DocumentId;
+         join Documents D on I.item_id = D.document_id;
 
 
-drop view if exists PersonView;
-create view PersonView as
-select I.ItemId as Id,
-       I.CreatedDateTime,
-       I.UpdatedDateTime,
-       P.FullName,
-       P.BirthDate,
+drop view if exists person_view;
+create view person_view as
+select I.item_id as Id,
+       I.created_at,
+       I.updated_at,
+       P.full_name,
+       P.birth_date,
        P.Description
 from Items I
-         join People P on I.ItemId = P.PersonId;
+         join People P on I.item_id = P.person_id;
 
 
 -- REFERENCE VIEW - THE ONLY RIGHT WAY TO ACCESS REFERENCES ;)
-drop view if exists ReferenceView;
-create view ReferenceView as
-select I.ItemId    as Id,
-       IT.TypeCode as Type,
+drop view if exists reference_view;
+create view reference_view as
+select I.item_id    as Id,
+       IT.type_code as Type,
        S.StoryDateTime,
        S.Header    as StoryHeader,
        F.Title     as FileTitle,
-       P.FullName  as PersonName,
+       P.full_name  as PersonName,
        D.Title     as DocumentTitle,
        P2.Title    as PhotoTitle
 from Items I
-         join Type IT on I.TypeCode = IT.TypeCode
-         left join Story S on S.StoryId = I.ItemId
-         left join People P on I.ItemId = P.PersonId
-         left join Documents D on I.ItemId = D.DocumentId
-         left join Photos P2 on I.ItemId = P2.PhotoId
-         left join Files F on F.FileId = I.ItemId;
+         join Type IT on I.type_code = IT.type_code
+         left join Story S on S.story_id = I.item_id
+         left join People P on I.item_id = P.person_id
+         left join Documents D on I.item_id = D.document_id
+         left join Photos P2 on I.item_id = P2.photo_id
+         left join Files F on F.file_id = I.item_id;
 
 -- AND VERY USEFUL HELPER VIEWS
-drop view if exists ReferencedIn;
-create view ReferencedIn as
+drop view if exists referenced_in;
+create view referenced_in as
 select RV.*,
-       R.ReferenceId,
-       R.TargetId
-from ReferenceView RV
-         join Reference R on R.SourceId = RV.Id;
+       R.reference_id,
+       R.target_id
+from reference_view RV
+         join Reference R on R.source_id = RV.Id;
 
-drop view if exists ReferencesTo;
-create view ReferencesTo as
+drop view if exists references_to;
+create view references_to as
 select RV.*,
-       R.ReferenceId,
-       R.SourceId
-from ReferenceView RV
-         join Reference R on R.TargetId = RV.Id;
+       R.reference_id,
+       R.source_id
+from reference_view RV
+         join Reference R on R.target_id = RV.Id;
 
 create or replace function reference_targets(source_id uuid)
-    returns setof ReferencesTo
+    returns setof references_to
     language plpgsql
 as
 $$
 begin
-    return query select * from referencesto where SourceId = source_id;
+    return query select * from references_to where source_id = source_id;
 end;
 $$;
 
 create or replace function reference_sources(target_id uuid)
-    returns setof ReferencesTo
+    returns setof references_to
     language plpgsql
 as
 $$
 begin
-    return query select * from ReferencedIn where TargetId = target_id;
+    return query select * from referenced_in where target_id = target_id;
 end;
 $$
